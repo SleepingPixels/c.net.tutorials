d7c1253ce66dc7aa30ffb01ff118a42434778a6f
Jessica Oxley <jessa.oxley@gmail.com>
2025-06-30 21:56:00 +0800

    Completed 1st module in C# .Net training.

--- c#tutorial.cs
+++ c#tutorial.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello World and hello Jess!");
+using System.Diagnostics;
+
+Console.WriteLine("Congratulations!");
+Console.WriteLine("You wrote your first lines of code.");
+Console.WriteLine(" ");
+
+//TUTORIAL NOTES
+//Console: is the container known as a "Class"
+//. is the operator that connects a method to a class
+//WriteLine() is a method that is found within a class, can be thought of like a function in Python. A method always has () parentheses. 
+//Syntax is CaseSensitive and each line ends with a ; to signify the end of the command. Must also use double quotation marks "" unlike Python which can use either if used consistently. 
+
+//Console.Write("") concatonates strings together, a manual space is needed for this to be readible. 
+//Console.WriteLine("") outputs the string as requested, and then automatically inserts a new line for the next command.
+
+Console.Write("Unit 4 of 7 - Write code challenge");
+Console.WriteLine(" https://learn.microsoft.com/en-us/training/modules/csharp-write-first/4-challenge");
+Console.WriteLine(" ");
+Console.WriteLine("This is the first line.");
+Console.Write("This is the second line.");
--- obj/Debug/net10.0/c#tutorial.AssemblyInfo.cs
+++ obj/Debug/net10.0/c#tutorial.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#tutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55704b729fcdb3ed13eeea324890de296fd1e954")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#tutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#tutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
